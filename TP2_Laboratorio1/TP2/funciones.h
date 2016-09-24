3d071b0f5a57a5c52f754eed8f6a026c406ed98b
MarianoLoPrete <marianoloprety@gmail.com>
2016-09-23 23:26:35 -0300

    Add files via upload

---
+++ TP2_Laboratorio1/TP2/funciones.h
@@ -0,0 +1,54 @@
+#ifndef FUNCIONES_H_INCLUDED
+#define FUNCIONES_H_INCLUDED
+#include "funciones.c"
+#include "funciones.h"
+
+
+
+/**
+ * Obtiene el primer indice libre del array.
+ * @param lista el array se pasa como parametro.
+ * @return el primer indice disponible
+ */
+int obtener(personas lista[],int recorre);
+
+
+/**
+ * Inicializa todas las operaciones
+ * @param lista estado le asigna el valor cero
+ */
+
+void iniciar(personas lista[],int recorre);
+
+/**
+ * Da de alta un usuario en diferentes posiciones del array Lista
+ * @param Pide datos de persona
+ * @param Asigna valores a posiciones de array
+ */
+void alta(personas lista[],int recorre);
+
+
+/**
+ * Muestra una lista ordenada de personas
+ * @param Recorre el array lista buscando los nombres
+ * @param Acomoda alfabeticamente los datos
+ * @return Muestra los nombres de manera ordenada
+ */
+void listado(personas lista[],int recorre);
+
+/**
+ * Muestra una Grafico ordenada de personas
+ * @param Recorre el array lista buscando los las edades
+ * @param Acomoda los datos en columnas
+ * @return Muestra las edades
+ */
+void grafico(personas lista[]);
+
+/**
+ * Muestra una Grafico ordenada de personas
+ * @param Recorre el array lista buscando los nombres
+ * @param Acomoda alfabeticamente los datos
+ * @return Muestra los nombres de manera ordenada
+ */
+
+#endif // FUNCIONES_H_INCLUDED
